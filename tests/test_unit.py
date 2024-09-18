f1ba459fe6510dc906278b0ce74782ea0a3323d8
EclipseZ23 <nosc22vy@student.ju.se>
2024-09-18 15:24:54 +0200

    test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -15,52 +15,44 @@ class TestBase():
         cls.calculator = None
 
 
-    class TestCalculator():
+class TestCalculator(TestBase):
+    
+    @pytest.mark.parametrize("a, b, expected", [
+        (9, -5, 4),
+        (2, 3, 5),
+        (-1, 1, 0),
+        (0, 0, 0)
+    ])
+
+    def test_add(self, a, b, expected):
         
-        @pytest.mark.parametrize("a, b, expected", [
-            (9, -5, 4),
-            (2, 3, 5),
-            (-1, 1, 0),
-            (0, 0, 0)
-        ])
-
-        def test_add(self, a, b, expected):
-            # Arrange
-            calculator = CalculatorHelper()
-            # Action
-            value = calculator.add(a, b)
-            # Assert
-            assert value == expected, f"Expected {a} + {b} to be {expected}, but got {value}"
+        # Action
+        value = self.calculator.add(a, b)
+        # Assert
+        assert value == expected, f"Expected {a} + {b} to be {expected}, but got {value}"
 
 
-        def test_subtract(self):
-            # Arrange
-            calculator = CalculatorHelper()
-            # Action
-            value = calculator.subtract(1,1)
-            # Assert
-            assert value == 0, "Expected result to be 0"
-        
-        def test_divide(self):
-            # Arrange
-            calculator = CalculatorHelper()
-            # Action
-            value = calculator.divide(10,2)
-            # Assert
-            assert value == 5, "Expected result to be 5"
+    def test_subtract(self):
+    
+        # Action
+        value = self.calculator.subtract(1,1)
+        # Assert
+        assert value == 0, "Expected result to be 0"
+    
+    def test_divide(self):
 
-        def test_multiply(self):
-            # Arrange
-            calculator = CalculatorHelper()
-            # Action
-            value = calculator.multiply(1,1)
-            # Assert
-            assert value == 1, "Expected result to be 1"
+        # Action
+        value = self.calculator.divide(10,2)
+        # Assert
+        assert value == 5, "Expected result to be 5"
 
-        def test_div_by_zero(self):
-            with pytest.raises(ZeroDivisionError):
-                1 / 0
+    def test_multiply(self):
+      
+        # Action
+        value = self.calculator.multiply(1,1)
+        # Assert
+        assert value == 1, "Expected result to be 1"
 
-            #my_name = "Scott"
-            #assert_that(value).is_equal_to(2)
-            #assert_that(my_name).is_length(5).starts_with("S").ends_with("tt")
+    def test_div_by_zero(self):
+        with pytest.raises(ZeroDivisionError):
+            pytest.mark.parametrize(self.calculator.divide(10,0))
